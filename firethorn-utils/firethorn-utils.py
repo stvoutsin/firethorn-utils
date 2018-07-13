e2c7fc5e805bf5fe0bf8dbd0c34d5e0ad43d9d0f
Stelios Voutsinas <steliosvoutsinas@yahoo.com>
2018-07-13 21:32:28 +0300

    Initial code for Configurator (In progress)

---
+++ firethorn-utils/firethorn-utils.py
@@ -0,0 +1,26 @@
+try:
+    import logging
+    from src import *
+    
+except Exception as e:
+    logging.exception(e)
+
+
+class FirethornUtils(object):
+    """
+    Firethorn Utility class
+    Query WFAU and VO services
+        
+    """
+
+    
+    def __init__(self):
+        return        
+
+
+    
+    
+
+    
+
+
---
+++ setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "firethorn-utils",
+    version = "0.1.0",
+    author = "Stelios Voutsinas",
+    author_email = "stv@roe.ac.uk",
+    description = ("A python suite for utility functions for firethorn"),
+    license = "BSD",
+    keywords = "firethorn vo",
+    url = "http://wfau.metagrid.co.uk/code/firethorn",
+    include_package_data = True,  
+    packages=['firethorn-utils', 'firethorn-utils.src'],
+    long_description=read('README'),
+    classifiers=[
+        "Development Status :: 1 - Alpha",
+        "Topic :: Utilities",
+        "License :: GPL License",
+    ],
+    install_requires=[
+        'numpy>=1.4.0',
+        'astropy>=0.4.1',
+        'pyodbc>=4.0.21',
+        'pyparsing>=2.2.0',
+        'pycurl>=7.43.0'
+    ]
+)
+
