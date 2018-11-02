738c8e30de598f47ca594ab0bc13fe97e453bd29
Stelios Voutsinas <steliosvoutsinas@yahoo.com>
2018-11-02 18:51:41 +0200

    Updating Validator code, added Validator Results class and commandline arguments in validator main class

--- firethorn_utils/validator.py
+++ firethorn_utils/validator.py
@@ -15,27 +15,96 @@ try:
     from firethorn.models.adql.adql_resource import AdqlResource 
     import sys
     import uuid
+    import time
+    from argparse import ArgumentParser
+
 except Exception as e:
     logging.exception(e)
 
 
+
+class ValidatorResults(object):
+    '''
+    Validator Results Class, stores information from a validation run
+    '''
+
+    def __init__(self, processed, candidates, exceptions, total_time = 0):
+        '''
+        Constructor
+        '''
+        self.processed = processed
+        self.candidates = candidates
+        self.exceptions = exceptions
+        self.total_time = 0
+
+        return
+
+
+    @property
+    def processed(self):
+        return self.__processed
+        
+        
+    @processed.setter
+    def processed(self, processed):
+        self.__processed = processed
+
+
+    @property
+    def candidates(self):
+        return self.__candidates
+
+
+    @candidates.setter
+    def candidates(self, candidates):
+        self.__candidates = candidates
+
+
+    @property
+    def exceptions(self):
+        return self.__exceptions
+
+
+    @exceptions.setter
+    def exceptions(self, exceptions):
+        self.__exceptions = exceptions
+
+
+    @property
+    def total_time(self):
+        return self.__total_time
+
+
+    @total_time.setter
+    def total_time(self, total_time):
+        self.__total_time = total_time
+
+
+
+
+
 class Validator(object):
     '''
-    classdocs
+    Validator class, used to validate an AdqlResource
     '''
 
 
-    def __init__(self, firethorn_object ):
+    def __init__(self, firethorn_object , verbose):
         '''
         Constructor
         '''
         self.firethorn_object = firethorn_object
+        self.verbose = verbose
         return
      
+
     def validate (self, resource_url):
         '''
         Validate an Adql Resource, Check all tables by querying and checking result row count
         '''
+
+        start_time = time.time()
+        print ("--- Starting validation on Resource: " + resource_url + "---")
 
         acc = self.firethorn_object.identity()
 
@@ -58,11 +127,12 @@ class Validator(object):
         for schema in resource.get_schemas():
             for table in schema.get_tables():
                 fullname = schema.name() + "." + table.name()
-                print(
-                    "Testing [{}]".format(
-                        fullname
+                if (self.verbose==True):
+                    print(
+                        "Testing [{}]".format(
+                            fullname
+                            )
                         )
-                    )
                 try:
                     query_str = "SELECT TOP 10 * FROM {}.{}".format(
                         schema.name(),
@@ -71,24 +141,78 @@ class Validator(object):
                     query_obj = resource.query(
                         query_str
                         )
-                    py_table = query_obj.results().as_astropy()
-                    py_table.pprint()
+
+
+                    #py_table = query_obj.results().as_astropy()
+                    #py_table.pprint()
                     rowcount = query_obj.results().rowcount()
                     processed[fullname] = rowcount
                     if (rowcount < 10):
                         candidates[fullname] = rowcount
-                        print(
-                            "Candidate [{}] [{}]".format(
-                                fullname,
-                                rowcount
+                        if (self.verbose==True):
+                            print(
+                                "Candidate [{}] [{}]".format(
+                                    fullname,
+                                    rowcount
+                                    )
                                 )
-                            )
                 except:
                     message = sys.exc_info()[0]
                     exceptions[fullname] = message
-                    print(
-                    "Exception [{}] [{}]".format(
-                        fullname,
-                        message
-                        )
-                    )
+                    if (self.verbose==True):
+                        print(
+                           "Exception [{}] [{}]".format(
+                                fullname,
+                                message
+                                )
+                            )
+
+        total_time = time.time() - start_time 
+        print("--- Validation completed after  %s seconds ---" % (total_time))
+       
+        return  ValidatorResults(candidates=candidates, processed=processed, exceptions=exceptions, total_time=total_time)
+
+
+
+
+if __name__ == "__main__":
+
+    '''
+    Validator Arguments
+    '''
+    parser = ArgumentParser()
+    parser.add_argument("-ft", "--firethorn_url", dest="firethorn_url",
+                    help="Firethorn Instance URL", metavar="Firethorn")
+    parser.add_argument("-r", "--resource_id", dest="resource_id",
+                    help="Resource ID", metavar="RESOURCE")
+    parser.add_argument("-u", "--username", dest="username",
+                    help="Firethorn username", metavar="USERNAME")
+    parser.add_argument("-p", "--pass", dest="password",
+                    help="Firethorn password", metavar="PASSWORD")
+    parser.add_argument("-g", "--group", dest="group",
+                    help="Firethorn group", metavar="GROUP")
+    parser.add_argument("-v", "--verbose", dest="verbose", default=True,
+                    help="Print status messages to stdout")
+    args = parser.parse_args()    
+
+
+
+    ft = firethorn.Firethorn(endpoint=args.firethorn_url)
+    ft.login(args.username, args.password, args.group)
+
+    validator_obj = Validator(ft, args.verbose)
+    validator_results = validator_obj.validate(args.firethorn_url + "/adql/resource/"  + args.resource_id)
+    
+
+    print ("Processed: ")
+    print (validator_results.processed)
+    print ("------------")
+ 
+    print ("Candidates: ")
+    print (validator_results.candidates)
+    print ("------------")
+
+
+    print ("Exceptions: ")
+    print (validator_results.exceptions)
+    print ("------------")
--- tests/testValidator.py
+++ tests/testValidator.py
@@ -6,12 +6,33 @@ Created on Jul 17, 2018
 
 import firethorn_utils.validator as validator
 import firethorn
+from argparse import ArgumentParser
 
 if __name__ == "__main__":
-    
-    ft = firethorn.Firethorn(endpoint="http://tap.metagrid.xyz/firethorn")
-    print (ft.identity())
-    ft.login(firethorn.config.adminuser, firethorn.config.adminpass, firethorn.config.admingroup)
-    print (ft.identity())
-    testValidator = validator.Validator(ft)
-    testValidator.validate("http://tap.metagrid.xyz/firethorn/adql/resource/54")
+
+    '''
+    Validator Arguments
+    '''
+    parser = ArgumentParser()
+    parser.add_argument("-ft", "--firethorn_url", dest="firethorn_url",
+                    help="Firethorn Instance URL", metavar="Firethorn")
+    parser.add_argument("-r", "--resource_id", dest="resource_id",
+                    help="Resource ID", metavar="RESOURCE")
+    parser.add_argument("-u", "--username", dest="username",
+                    help="Firethorn username", metavar="USERNAME")
+    parser.add_argument("-p", "--pass", dest="password",
+                    help="Firethorn password", metavar="PASSWORD")
+    parser.add_argument("-g", "--group", dest="group",
+                    help="Firethorn group", metavar="GROUP")
+    parser.add_argument("-v", "--verbose", dest="verbose", default=True,
+                    help="Print status messages to stdout")
+    args = parser.parse_args()
+
+
+
+    ft = firethorn.Firethorn(endpoint=args.firethorn_url)
+    ft.login(args.username, args.password, args.group)
+
+    validator_obj = validator.Validator(ft, args.verbose)
+    validator_obj.validate(args.firethorn_url + "/adql/resource/"  + args.resource_id)
+
